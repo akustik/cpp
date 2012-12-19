17d31f7fa245bc89327bfd531d167242af9616bc
akustik <guillem.mercadal@gmail.com>
2012-12-19 17:26:39 +0100

    Added command line tool for testing

--- helloworld/src/helloworld.cpp
+++ helloworld/src/helloworld.cpp
@@ -7,20 +7,53 @@
 //============================================================================
 
 #include <iostream>
+#include <sstream>
+#include <string>
 #include "FizzBuzz.h"
 #include "FizzBuzzResult.h"
 
 using namespace std;
 using namespace fizzbuzz;
 
-int main(int argc, char **argv) {
+void doRaw(){
 	FizzBuzz fizzBuzz;
-	FizzBuzzResult* vectorResults = fizzBuzz.execute();
-	vectorResults->print();
-
 	FizzBuzzResult* rawResults =fizzBuzz.executeWithArrays();
-	rawResults->print();
-
-	delete vectorResults;
 	delete rawResults;
 }
+
+void doVector(){
+	FizzBuzz fizzBuzz;
+	FizzBuzzResult* vectorResults =fizzBuzz.execute();
+	delete vectorResults;
+}
+
+int main(int argc, char **argv) {
+	if(argc == 3){
+		string executionType(argv[1]);
+		string sTimes(argv[2]);
+
+		unsigned int iTimes;
+		istringstream ( sTimes ) >> iTimes;
+
+		if(executionType.compare(string("VECTOR")) == 0){
+			cout << "Computing vector fizzbuzz " << iTimes << " times" << endl;
+			for(unsigned int i=0; i<iTimes; i++){
+				doRaw();
+			}
+		} else if (executionType.compare(string("RAW")) == 0){
+			cout << "Computing raw fizzbuzz " << iTimes << " times" << endl;
+			for(unsigned int i=0; i<iTimes; i++){
+				doVector();
+			}
+		} else {
+			cout << "Usage: <type: VECTOR or RAW> <times>: "
+					<< "Unknown type: " << executionType  << endl;
+		}
+	} else {
+		cout << "Usage: <type: VECTOR or RAW> <times>" << endl;
+	}
+
+	return 0;
+}
+
+
