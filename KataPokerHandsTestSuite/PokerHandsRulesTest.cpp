fc0a2d8786fd51cdacf7f411df37bf92f3eca108
akustik <guillem.mercadal@gmail.com>
2012-12-25 21:50:43 +0100

    Added first test situation for a higher card case

--- KataPokerHandsTestSuite/PokerHandsRulesTest.cpp
+++ KataPokerHandsTestSuite/PokerHandsRulesTest.cpp
@@ -18,6 +18,12 @@ TEST(PokerHandsRulesSpecs, ShouldReturnAnErrorMessageWhenAnUnknownInputIsSubmit)
 	ASSERT_EQ(string("Unknown situation"), decision) << "The error message does not match";
 }
 
+TEST(PokerHandsRulesSpecs, ShouldWinWhiteWithHighCardAce){
+	PokerHandsRules rules;
+	string decision = rules.chooseWinner(string("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH"));
+	ASSERT_EQ(string("White wins. - with high card: Ace"), decision) << "The decision is not correct";
+}
+
 
 
 
