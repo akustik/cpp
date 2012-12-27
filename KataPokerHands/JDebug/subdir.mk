83f42baf36bc2dd334ba60d423873eacb0503552
akustik <guillem.mercadal@gmail.com>
2012-12-28 00:00:53 +0100

    Added JDebug directory

---
+++ KataPokerHands/JDebug/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../DelegatedPokerDecisionMaker.cpp \
+../FlashPokerDecisionMaker.cpp \
+../HighCardPokerDecisionMaker.cpp \
+../PokerCard.cpp \
+../PokerDecisionMaker.cpp \
+../PokerHand.cpp \
+../PokerHandParser.cpp \
+../PokerHandsRules.cpp \
+../StraightFlashPokerDecisionMaker.cpp 
+
+OBJS += \
+./DelegatedPokerDecisionMaker.o \
+./FlashPokerDecisionMaker.o \
+./HighCardPokerDecisionMaker.o \
+./PokerCard.o \
+./PokerDecisionMaker.o \
+./PokerHand.o \
+./PokerHandParser.o \
+./PokerHandsRules.o \
+./StraightFlashPokerDecisionMaker.o 
+
+CPP_DEPS += \
+./DelegatedPokerDecisionMaker.d \
+./FlashPokerDecisionMaker.d \
+./HighCardPokerDecisionMaker.d \
+./PokerCard.d \
+./PokerDecisionMaker.d \
+./PokerHand.d \
+./PokerHandParser.d \
+./PokerHandsRules.d \
+./StraightFlashPokerDecisionMaker.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/usr/include/c++/4.6 -I/usr/include/c++/4.6/i686-linux-gnu -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
